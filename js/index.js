a2ff47c52da457f21b5d47424dba06380bcda5a2
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-03-20 14:52:30 +0100

    Add operate function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
 function add(a , b) {
     return a + b;
 }
@@ -13,4 +17,16 @@ function multiply(a, b) {
 function divide(a, b) {
     if(b === 0) return `Can't divide by 0`;
     return a / b;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+    if(operator === '+') {
+        return add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        return substract(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        return divide(firstNumber, secondNumber);
+    }
 }
