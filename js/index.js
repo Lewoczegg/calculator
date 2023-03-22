8994b64ade049bea45e4cc7c4fdd35bb28c82cf1
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-03-22 19:59:05 +0100

    Write first number on second line

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-let firstNumber;
-let secondNumber;
+let firstNumber = '';
+let secondNumber = '';
 let operator;
+let isNumberWithComa = false;
+
+const displayFirstLine = document.querySelector('.display-first-line');
+displayFirstLine.textContent = '543534';
+
+const displaySecondLine = document.querySelector('.display-second-line');
+displaySecondLine.textContent = '0';
 
 function add(a , b) {
     return a + b;
@@ -28,5 +35,32 @@ function operate(firstNumber, operator, secondNumber) {
         return multiply(firstNumber, secondNumber);
     } else if (operator === '/') {
         return divide(firstNumber, secondNumber);
+    }
+}
+
+let numberButtons = document.querySelectorAll('.btn-number');
+numberButtons.forEach(button => button.addEventListener('click', clickNumberButton));
+
+function clickNumberButton() {
+    let digit = this.textContent;
+    if(operator === undefined && digit === '0' && firstNumber === '') return;
+    if(operator === undefined) {
+        firstNumber += digit;
+        displaySecondLine.textContent = firstNumber;
+    }
+}
+
+let dotButton = document.querySelector('.btn-dot');
+dotButton.addEventListener('click', clickDotButton);
+
+function clickDotButton() {
+    if(isNumberWithComa === false && operator === undefined && firstNumber === '') {
+        firstNumber += '0.';
+        displaySecondLine.textContent = firstNumber;
+        isNumberWithComa = true;
+    } else if (isNumberWithComa === false && operator === undefined) {
+        firstNumber += '.';
+        displaySecondLine.textContent = firstNumber;
+        isNumberWithComa = true;
     }
 }
